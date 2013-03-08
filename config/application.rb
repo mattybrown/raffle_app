e5259248f33e3554e9c027b7db62e6ae1050bb11
Matt Brown <brownprobable@gmail.com>
2013-03-08 16:53:56 +1300

    re-precompiled... work goddamnit

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module RaffleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+	
+	config.assets.precompile += ['print.css.scss']
   end
 end
